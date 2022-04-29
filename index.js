e54c5d32d66dfa263d0d29a46ab24132fa60daf4
felipepineda40academy <felipe.pineda@academiacuatropuntocero.com>
2022-04-28 20:26:55 -0600

    trabajo en clase 27-8

---
+++ index.js
@@ -0,0 +1,11 @@
+//1. Hacer un if con una variable let 
+
+//2. Hacer una funcion que me diga 'Hola Profe, vea que chiva funcion'
+
+//3. Hacer una funcion que me sume 3 numeros (tienen que usar argumentos)
+
+//4. Hacer una funcion que reciba una variable llamada var me_invitaron, y esa funcion responde 'eehhh me invitaron' o 'que mal no me invitaron'
+
+//5. Hacer una funcion que multiplique 3 numeros (tienen que usar argumentos)
+
+
